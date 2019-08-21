5295ac7f7f26c3a43f0fe9b9c826e876b29cf666
Avi Dessauer <avi.the.coder@gmail.com>
2019-08-21 05:59:18 -0400

    Bump 8.6.5 plans to lts-14 (#41)

--- app/Server.hs
+++ app/Server.hs
@@ -125,7 +125,11 @@ import Servant
   , Header
   , Headers
   , QueryParam
+#if MIN_VERSION_GLASGOW_HASKELL(8,6,5,0)
+  , ServerError
+#else
   , ServantErr
+#endif
   , ServerT
   , ToHttpApiData(..)
   , addHeader
@@ -1835,7 +1839,11 @@ fileNotFound :: Response
 fileNotFound =
   responseLBS status404 [("Content-Type", "text/plain")] "Not found"
 
+#if MIN_VERSION_GLASGOW_HASKELL(8,6,5,0)
+throwServantError :: (MonadIO m) => ServerError -> m a
+#else
 throwServantError :: (MonadIO m) => ServantErr -> m a
+#endif
 throwServantError = liftIO . throwIO 
 
 server :: Environment -> ServerT API Handler
